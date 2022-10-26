c6567f5d1c42879ef4485bc24d855a444aefd27c
Octavian <sirghi1979@mail.ru>
2022-10-26 14:55:54 +0300

    Changelog 25.10.2022 день

--- src/pages/cabinet/products/AddProduct.jsx
+++ src/pages/cabinet/products/AddProduct.jsx
@@ -52,14 +52,20 @@ export default function AddProduct() {
                 <div className="flex flex-col lg:flex-row mb-3">
                     <div className="space-y-4 2xl:w-4/5 lg:w-3/5">
                         <span className="text-2xl font-semibold">О товаре</span>
-                        <div className="flex-col ml-2 space-y-2">
-                            <div className="flex flex-col sm:flex-row">
-                                <span className="mr-2">Категория товара</span>
+                        <div className="flex-col ml-2 space-y-3">
+                            <div className="flex flex-col justify-center items-start">
+                                <label
+                                    htmlFor="productCategory"
+                                    className="mr-2 mb-1 font-semibold"
+                                >
+                                    Категория товара
+                                </label>
                                 <select
+                                    id="productCategory"
                                     {...register("category", {
                                         required: "Выберите категорию товара",
                                     })}
-                                    className="bg-transparent border-b-2 dark:border-b-white border-b-black dark:focus:border-b-orange-500 focus:border-b-orange-500 duration-300 outline-none pr-3 cursor-pointer"
+                                    className="bg-transparent border-2 px-2 py-1 rounded-lg border-zinc-700 focus:border-orange-500 hover:border-zinc-500 duration-200 outline-none pr-3 cursor-pointer"
                                 >
                                     <option value="" disabled>
                                         Выберите категорию
@@ -74,7 +80,7 @@ export default function AddProduct() {
                                 </span>
                             )}
                             <div className="flex flex-col">
-                                <span>Пол</span>
+                                <span className="mb-1 font-semibold">Пол</span>
                                 <label
                                     className="relative w-fit"
                                     style={{ cursor: "pointer" }}
@@ -101,18 +107,22 @@ export default function AddProduct() {
                                     Женский
                                 </label>
                             </div>
-                            <div className="flex flex-col">
-                                <div className="flex flex-col sm:flex-row">
-                                    <span className="mr-2">
+                            <div className="flex flex-col justify-start">
+                                <div className="flex flex-col justify-center items-start">
+                                    <label
+                                        htmlFor="productName"
+                                        className="mr-2 mb-1 font-semibold"
+                                    >
                                         Название товара
-                                    </span>
+                                    </label>
                                     <input
                                         type="text"
+                                        id="productName"
                                         {...register("productName", {
                                             required:
                                                 "Необходимо указать название товара",
                                         })}
-                                        className="bg-transparent border-b-2 dark:border-b-white border-b-black dark:focus:border-b-orange-500 focus:border-b-orange-500 duration-300"
+                                        className="bg-transparent border-2 rounded-lg sm:w-64 px-2 py-1 border-zinc-700 focus:border-orange-500 hover:border-zinc-500 duration-200"
                                     />
                                 </div>
                                 {errors?.productName && (
@@ -122,13 +132,19 @@ export default function AddProduct() {
                                 )}
                             </div>
                             <div className="flex flex-col">
-                                <span>Описание товара</span>
+                                <label
+                                    htmlFor="description"
+                                    className="font-semibold mb-1"
+                                >
+                                    Описание товара
+                                </label>
                                 <textarea
+                                    id="description"
                                     {...register("description", {
                                         required: "Поле не может быть пустым",
                                     })}
                                     rows="5"
-                                    className="lg:w-4/5 mt-2 bg-transparent border-2 dark:border-white border-black rounded-xl px-3 py-2 dark:focus:border-orange-500 focus:border-orange-500 outline-none"
+                                    className="lg:w-4/5 bg-transparent border-2 rounded-lg px-2 py-1 border-zinc-700 focus:border-orange-500 hover:border-zinc-500 duration-200 outline-none"
                                     placeholder="Укажите описание товара"
                                 ></textarea>
                                 {errors?.description && (
@@ -142,15 +158,21 @@ export default function AddProduct() {
                             Размеры и количество
                         </div>
                         <div className="flex flex-col items-start ml-2">
-                            <div className="flex">
-                                <span className="mr-2">Размер</span>
+                            <div className="flex flex-col justify-center">
+                                <label
+                                    htmlFor="size"
+                                    className="mr-2 mb-1 font-semibold"
+                                >
+                                    Размер
+                                </label>
                                 <select
+                                    id="size"
                                     {...register("size", {
                                         required: "Выберите размер",
                                     })}
-                                    className="bg-transparent border-b-2 text-center dark:border-b-white border-b-black pr-2 dark:focus:border-b-orange-500 focus:border-b-orange-500"
+                                    className="bg-transparent border-2 px-2 py-1 rounded-lg text-center border-zinc-700 focus:border-orange-500 hover:border-zinc-500 duration-200 cursor-pointer"
                                 >
-                                    <option value="">&mdash;</option>
+                                    <option value="">Выберите размер</option>
                                     <option value="s">S</option>
                                     <option value="m">M</option>
                                     <option value="l">L</option>
@@ -165,9 +187,15 @@ export default function AddProduct() {
                                     {errors.size.message}
                                 </span>
                             )}
-                            <div className="flex flex-col sm:flex-row mt-2">
-                                <span className="mr-2">Количество</span>
+                            <div className="flex flex-col justify-center items-start mt-2">
+                                <label
+                                    htmlFor="sizeCount"
+                                    className="mr-2 mb-1 font-semibold"
+                                >
+                                    Количество (шт.)
+                                </label>
                                 <input
+                                    id="sizeCount"
                                     type="number"
                                     {...register("count", {
                                         required: "Поле не должно быть пустым",
@@ -175,7 +203,7 @@ export default function AddProduct() {
                                             value: /[0-9]+/gi,
                                         },
                                     })}
-                                    className="bg-transparent border-b-2 dark:border-b-white border-b-black pr-2 w-fit dark:focus:border-b-orange-500 focus:border-b-orange-500"
+                                    className="bg-transparent border-2 px-2 py-1 rounded-lg border-zinc-700 focus:border-orange-500 hover:border-zinc-500 duration-200"
                                 />
                             </div>
                             {errors?.count && (
@@ -183,16 +211,20 @@ export default function AddProduct() {
                                     {errors.count.message}
                                 </span>
                             )}
-                            <button className="mt-3 bg-yellow-500 hover:bg-yellow-600 px-4 py-1 font-semibold text-center rounded-2xl ">
+                            <button className="mt-3 bg-yellow-500 hover:bg-yellow-600 px-4 py-1 font-semibold text-center rounded-2xl">
                                 Добавить размер
                             </button>
                         </div>
                         <div className="text-2xl font-semibold">Цена</div>
-                        <div className="flex flex-col sm:flex-row">
-                            <span className="mr-2">
+                        <div className="flex flex-col justify-center items-start">
+                            <label
+                                htmlFor="price"
+                                className="mr-2 mb-1 font-semibold"
+                            >
                                 Стоимость за шт. (руб.)
-                            </span>
+                            </label>
                             <input
+                                id="price"
                                 type="number"
                                 {...register("price", {
                                     required: "Необходимо указать стоимость",
@@ -200,7 +232,7 @@ export default function AddProduct() {
                                         value: /[0-9]+/gi,
                                     },
                                 })}
-                                className="bg-transparent border-b-2 dark:border-b-white border-b-black pr-2 w-fit dark:focus:border-b-orange-500 focus:border-b-orange-500"
+                                className="bg-transparent border-2 px-2 py-1 rounded-lg border-zinc-700 focus:border-orange-500 hover:border-zinc-500 duration-200"
                             />
                         </div>
                         {errors?.price && (
